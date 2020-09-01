ae7408b243f7c6890be3d855f3d683a0b200c917
Seraphir <zhiyuandeng@126.com>
2020-09-01 11:51:28 +0800

    Update data_loaders.py

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -20,11 +20,13 @@ class MnistDataLoader(BaseDataLoader):
 
 class GestureDataLoader(BaseDataLoader):
 
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
+    def __init__(self, data_dir, index_file, batch_size, shuffle=True, validation_split=0.0, num_workers=1,
+                 training=True):
         trsfm = transforms.Compose([
             transforms.ToTensor(),
             # transforms.Normalize((0.1307,), (0.3081,))
         ])
         self.data_dir = data_dir
-        self.dataset = GestureData(self.data_dir, transform=trsfm)
+        self.index_file = index_file
+        self.dataset = GestureData(self.data_dir, self.index_file,transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
