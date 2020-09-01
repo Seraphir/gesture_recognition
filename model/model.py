97eaa3d1e4b4d3f35f814aa2b1623df4d5737d78
Seraphir <zhiyuandeng@126.com>
2020-09-01 13:20:55 +0800

    change model

--- model/model.py
+++ model/model.py
@@ -3,26 +3,54 @@ import torch.nn as nn
 import torch.nn.functional as F
 from base import BaseModel
 from torchvision.models import resnet50
-from torchvision.models import vgg16
-from .C3D_model import C3D
+
+cfgs = {
+    'A': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'B': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
+    'D': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
+    'E': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
+}
 
 
-class MergeNet(nn.Module):
+class C3DVGG(nn.Module):
 
-    def __init__(self, num_classes=3):
+    def __init__(self, num_classes=10, batch_norm=True, finetune=None):
         super().__init__()
-        self.net2d = vgg16(pretrained=False, num_classes=num_classes)
+        self.net2d = make_layers(cfgs['D'], batch_norm=batch_norm)
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
         self.lc2d = nn.Sequential(
-            nn.Linear(512 * 7 * 7, 4096),
+            nn.Linear(512 * 7 * 7, 2048),
             nn.ReLU(True)
         )
-        self.net3d = C3D(pretrained=False, num_classes=num_classes)
+        self.net3d = nn.Sequential(
+            nn.Conv3d(3, 64, kernel_size=(3, 3, 3), padding=(1, 1, 1)),
+            nn.ReLU(),
+            nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2)),
+            nn.Conv3d(64, 128, kernel_size=(3, 3, 3), padding=(1, 1, 1)),
+            nn.ReLU(),
+            nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2)),
+            nn.Conv3d(128, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1)),
+            nn.ReLU(),
+            nn.Conv3d(256, 256, kernel_size=(3, 3, 3), padding=(1, 1, 1)),
+            nn.ReLU(),
+            nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2)),
+            nn.Conv3d(256, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1)),
+            nn.ReLU(),
+            nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1)),
+            nn.ReLU(),
+            nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2)),
+            nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1)),
+            nn.ReLU(),
+            nn.Conv3d(512, 512, kernel_size=(3, 3, 3), padding=(1, 1, 1)),
+            nn.ReLU(),
+            nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2), padding=(0, 1, 1)),
+        )
         self.lc3d = nn.Sequential(
-            nn.Linear(8192, 4096),
+            nn.Linear(8192, 2048),
             nn.ReLU(True)
         )
         self.classifier = nn.Sequential(
-            nn.Linear(8192, 4096),
+            nn.Linear(4096, 4096),
             nn.ReLU(True),
             nn.Dropout(),
             nn.Linear(4096, 4096),
@@ -30,10 +58,14 @@ class MergeNet(nn.Module):
             nn.Dropout(),
             nn.Linear(4096, num_classes),
         )
+        self._initialize_weights()
+        if finetune is not None:
+            checkpoint = torch.load(finetune)
+            self.load_state_dict(checkpoint['state_dict'])
 
     def forward(self, x1, x2):
-        x1 = self.net2d.features(x1)
-        x1 = self.net2d.avgpool(x1)
+        x1 = self.net2d(x1)
+        x1 = self.avgpool(x1)
         x1 = torch.flatten(x1, 1)
         x1 = self.lc2d(x1)
         x2 = self.net3d.features(x2)
@@ -44,6 +76,39 @@ class MergeNet(nn.Module):
         # F.log_softmax(x, dim=1)
         return x
 
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.normal_(m.weight, 0, 0.01)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Conv3d):
+                torch.nn.init.kaiming_normal_(m.weight)
+            elif isinstance(m, nn.BatchNorm3d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+
+def make_layers(cfg, batch_norm=False):
+    layers = []
+    in_channels = 3
+    for v in cfg:
+        if v == 'M':
+            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+        else:
+            conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
+            if batch_norm:
+                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+            else:
+                layers += [conv2d, nn.ReLU(inplace=True)]
+            in_channels = v
+    return nn.Sequential(*layers)
 
 # if __name__ == "__main__":
 #     from torchviz import make_dot
