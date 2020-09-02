560042dcf5bbd0d8c182f396c3c5839a6cdfcd7e
Seraphir <zhiyuandeng@126.com>
2020-09-02 15:27:30 +0800

    Update index_merge.py

--- index_merge.py
+++ index_merge.py
@@ -1,7 +1,10 @@
+import os
+
+root_dir = ".\\data\\cropgest"
 index_list = ["index.txt", "index_50.txt", "index_oth.txt"]
-outfile = open('index_all.txt', 'w+')
+outfile = open(os.path.join(root_dir, 'index_all.txt'), 'w+')
 for index_file in index_list:
-    infile = open(index_file, 'r')
+    infile = open(os.path.join(root_dir, index_file), 'r')
     msg = infile.read()
     outfile.write(msg)
     infile.close()
